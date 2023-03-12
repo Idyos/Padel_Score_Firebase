f483697712c10f2b62ded8ace9299fd2f22013a1
Javi Gauxachs <javi.gauxachs@gmail.com>
2023-03-12 15:20:33 +0100

    puedes ver el orden de juegos en cada set

--- components/InfoPartida/JuegoDetalles.js
+++ components/InfoPartida/JuegoDetalles.js
@@ -1,27 +1,121 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { DataTable } from 'react-native-paper';
+import { StyleSheet, Text, View } from "react-native";
+import React, { useRef } from "react";
+import { ActivityIndicator, DataTable } from "react-native-paper";
 
-const JuegoDetalles = () => {
-  return (
-    <DataTable style={{ alignSelf: "center", width: "100%" }}>
-    <DataTable.Header>
-      <DataTable.Title>E1</DataTable.Title>
-    </DataTable.Header>
+const JuegoDetalles = ({
+  E1,
+  E2,
+  juego,
+  isVisible,
+  isLoaded,
+  pointsOnGame,
+}) => {
+  console.log(pointsOnGame[0]);
+  const pointsOnGameTeam1 = useRef(0);
+  const pointsOnGameTeam2 = useRef(0);
 
-    <DataTable.Row>
-        <DataTable.Cell><Text>HOLAAA</Text></DataTable.Cell>
-    </DataTable.Row>
+  pointsOnGameTeam1.current = 0;
+  pointsOnGameTeam2.current = 0;
 
-    <DataTable.Row style={{ justifyContent: "flex-start" }}>
-     <DataTable.Cell>Que tal o klk</DataTable.Cell>
-    </DataTable.Row>
-  </DataTable>
-  )
-}
+  const ScoreCorrecto = (team) => {
+    if(team==0){
+      switch (pointsOnGameTeam1.current) {
+        case 0:
+          pointsOnGameTeam1.current = 15;
+          break;
+          case 15:
+          pointsOnGameTeam1.current = 30;
+          break;
+          case 30:
+          pointsOnGameTeam1.current = 40;
+          break;
+          case 40:
+          pointsOnGameTeam1.current = "Win";
+          break;
+        }
+    }
+    if(team==1){
+      switch (pointsOnGameTeam2.current) {
+        case 0:
+          pointsOnGameTeam2.current = 15;
+          break;
+        case 15:
+          pointsOnGameTeam2.current = 30;
+          break;
+        case 30:
+          pointsOnGameTeam2.current = 40;
+          break; 
+        case 40:
+          pointsOnGameTeam2.current = "Win";
+          break;
+      }
+    }
+  };
 
-export default JuegoDetalles
+  return !isVisible ? (
+    ""
+  ) : !isLoaded ? (
+    <ActivityIndicator />
+  ) : (
+    <>
+      <Text>Juego {juego}</Text>
+      <DataTable style={{ alignSelf: "center", width: "100%" }}>
+        <DataTable.Header>
+          <DataTable.Title>{E1}</DataTable.Title>
+        </DataTable.Header>
+        <DataTable.Row>
+          {pointsOnGame[0].map((element, index) => {
+            if (element.team == "0") ScoreCorrecto(element.team);
+            return (
+              <DataTable.Cell
+                disabled={element.team == "0" ? false : true}
+                style={{ justifyContent: "center" }}
+                key={index}
+                numeric
+                onPress={() => console.log("ee")}
+                textStyle={
+                  element.team == "0" ? styles.wonGame : styles.lostGame
+                }
+              >
+                {pointsOnGameTeam1.current}
+              </DataTable.Cell>
+            );
+          })}
+        </DataTable.Row>
+        <DataTable.Header>
+          <DataTable.Title>{E2}</DataTable.Title>
+        </DataTable.Header>
+        <DataTable.Row>
+          {pointsOnGame[0].map((element, index) => {
+            if (element.team == "1") ScoreCorrecto(element.team);
+            return (
+              <DataTable.Cell
+                disabled={element.team == "1" ? false : true}
+                style={{ justifyContent: "center" }}
+                key={index}
+                numeric
+                onPress={() => console.log("ee")}
+                textStyle={
+                  element.team == "1" ? styles.wonGame : styles.lostGame
+                }
+              >
+                {pointsOnGameTeam2.current}
+              </DataTable.Cell>
+            );
+          })}
+        </DataTable.Row>
+      </DataTable>
+    </>
+  );
+};
 
-const styles = StyleSheet.create({ 
+export default JuegoDetalles;
 
-})
+const styles = StyleSheet.create({
+  wonGame: {
+    color: "black",
+  },
+  lostGame: {
+    opacity: 0.2,
+  },
+});
